de759904cd0550babb6be786d79220693a08a52e
Esther Agbaje <folasadeagbaje@gmail.com>
2022-10-16 02:31:16 +0400

    feat: added gradient background

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
-import { Container } from '@chakra-ui/react';
+import { Box, Container } from '@chakra-ui/react';
 import { About } from '../src/components/about';
 import { Footer } from '../src/components/footer';
 import { Projects } from '../src/components/projects';
 import { Tooling } from '../src/components/tooling';
 export default function Home() {
   return (
-    <Container maxW='container.lg' pt='16'>
-      <About />
-      <Projects />
-      <Tooling />
-      <Footer />
-    </Container>
+    <Box
+      position='relative'
+      backgroundImage='linear-gradient(70deg, rgb(248 228 260 / 24%) 0%, rgb(107 148 148 / 0%) 39%, rgb(166 236 250 / 13%) 100%);'
+    >
+      <Container maxW='container.lg' pt='16'>
+        <About />
+        <Projects />
+        <Tooling />
+        <Footer />
+      </Container>
+    </Box>
   );
 }
