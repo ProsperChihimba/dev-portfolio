1e50d1d114d31d5e2bfabdce7dfa04f42a4e204b
Esther Agbaje <folasadeagbaje@gmail.com>
2022-10-16 02:18:59 +0400

    feat: created tools and tooling component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import { Container } from '@chakra-ui/react';
 import { About } from '../src/components/about';
 import { Projects } from '../src/components/projects';
+import { Tooling } from '../src/components/tooling';
 export default function Home() {
   return (
     <Container maxW='container.lg' pt='16'>
       <About />
       <Projects />
+      <Tooling />
     </Container>
   );
 }
