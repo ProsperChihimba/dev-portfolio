2bef68f408f79de2a3510c05c705ad110a2cdf0e
Esther Agbaje <folasadeagbaje@gmail.com>
2022-10-17 19:45:31 +0400

    feat: added meta data

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import '@fontsource/figtree/400.css';
 
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
+import Head from 'next/head';
+
 const theme = extendTheme({
   fonts: {
     heading: `'Figtree', sans-serif`,
@@ -13,6 +15,10 @@ const theme = extendTheme({
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>Joke Bisman - Full Stack Developer</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
